efe9a779abd27c383d5b72cf834ca6f42ca820cf
localheist <gottingerfrank@zoho.com>
2016-12-04 09:51:13 +0100

    Added Battleship game and few minor other changes

--- classes_testdrive02.py
+++ classes_testdrive02.py
@@ -1,9 +1,8 @@
 ##################################################
 ###############   Coding Exercise  ###############
 ##################################################
-
-# Codecademy: Python Course(Python2) 
-# Classes - Test Drive (Test part2)
+ 
+# Classes - Test Drive (Test part2) (Python 3)
 
 ##################################################
 
@@ -13,20 +12,29 @@ class Animal(object):
     def __init__(self, name, age):
         self.name = name
         self.age = age
-       
     def description(self):   
         print (self.name)
         print (self.age)
-        
+    def health(self):
+        if self.is_alive:
+            return('Health is good')
+        else:
+            return('Not looking good...')
+    
 hippo = Animal("Berta", 45)
 
+if hippo.is_alive:
+    print('The fact that Hippo is alive is True !')
 print (hippo.description())
 
+print(hippo.health())
 
+sloth = Animal("fattycatty", 8)
+ocelot = Animal("graybeauty", 3)
 
-
-
-
+print(hippo.health)
+print(sloth.health)
+print(ocelot.health)
 
 ##################################################
 # END
